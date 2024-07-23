e4f58e6c8b8ffffd8e7c4f4458a649896c4a1ba6
abinayasivasub <142300983+abinayasivasub@users.noreply.github.com>
2024-07-23 13:25:30 +0530

    Update Git_ex1.java

--- src/basics/Git_ex1.java
+++ src/basics/Git_ex1.java
@@ -4,6 +4,8 @@ public class Git_ex1 {
 
 	public static void main(String[] args) {
 		System.out.println("Git Tutorial");
+		System.out.println("Git hub Tutorial");
+		System.out.println("Jenkins Tutorial");
 
 	}
 
